51f22f66ab6e041fee5696922f13a2fcf0954170
VIDUSHI GUPTA <vidugpt987@gmail.com>
2020-12-23 21:48:02 +0530

    jQuery AJAX request

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -7,8 +7,13 @@ $.ajax({
 	type:"GET",
 	url:"https://www.googleapis.com/books/v1/volumes?q=" + userSearch,
 	datatype:"JSON",
-	success: function(Book){
-		console.log(Book);
-	}
+	success: function(book){
+		console.log(book);
+		for(var i=0;i<book.items.length;i++){
+		var image = document.createElement('img');
+		image.src= book.items[i].volumeInfo.imageLinks.thumbnail;
+		document.getElementById('result').appendChild(image);
+		
+	}}
 })
 }
