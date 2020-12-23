021def09f301bbb619985358c90b5f7c17cfdb94
VIDUSHI GUPTA <vidugpt987@gmail.com>
2020-12-23 19:57:08 +0530

    some changes

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,4 +1,14 @@
 function findBook(){
 var userSearch = document.getElementById('userInput').value;
-document.getElementById('result').innerHTML = userSearch;
+var bookResult= document.getElementById('result');
+bookResult.innerHTML = "";
+
+$.ajax({
+	type:"GET",
+	url:"https://www.googleapis.com/books/v1/volumes?q=" + userSearch,
+	datatype:"JSON",
+	success: function(Book){
+		console.log(Book);
+	}
+})
 }
